5263fc77463db26fdd4d97aa9844231db64ca00c
Adam Herout <herout@vutbr.cz>
2025-04-07 09:53:10 +0200

    Added ease and clamped_linear functions

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,7 @@ The Malleable Glyph library.
 __author__ = 'Vojtech Bartl, Adam Herout'
 __credits__ = 'FIT BUT'
 
-from .mglyph import Canvas, export, interact, lerp, orbit, show, show_video, cubic_bezier_for_x
+from .mglyph import Canvas, export, interact, lerp, orbit, show, show_video, cubic_bezier_for_x, ease, clamped_linear
 
-__all__ = ['Canvas', 'export', 'interact', 'lerp', 'orbit', 'show', 'show_video', 'cubic_bezier_for_x']
+__all__ = ['Canvas', 'export', 'interact', 'lerp', 'orbit', 'show', 'show_video', 
+           'cubic_bezier_for_x', 'ease', 'clamped_linear']
