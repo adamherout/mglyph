2a3ee2fc262bd94490ec4e2c4b64dcd6280b075f
Adam Herout <herout@vutbr.cz>
2025-05-02 13:55:29 +0200

    Prepared for version 0.5.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / 'docs' / 'PyPI-info.md').read_text()
 
 setup(
     name='mglyph',
-    version='0.5.5',    
+    version='0.5.6',    
     description='The Malleable Glyph package',
     long_description=long_description,
     long_description_content_type='text/markdown',
